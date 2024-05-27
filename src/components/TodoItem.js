87f942080dbe259925b865b71fc11b0c2d2fd603
Kike <kike.mendoza9978@gmail.com>
2024-05-27 09:19:38 -0600

    Add new task to tasks array

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,14 @@
-export default function TodoItem(title) {
+export default function TodoItem(todo, onChange) {
     const article = document.createElement("article")
+    article.setAttribute("data-todo", todo.id)
 
     article.innerHTML = `
 
-    <label> <input type="checkbox"> ${title}<label/>
+    <label > <input type="checkbox" ${todo.isCompleted && "checked"}> ${todo.title}<label/>
     `
+
+    const checkbox = article.querySelector("input")
+
+    checkbox.addEventListener("change", (e) => onChange(e.target.checked))
     return article
 }
--- src/index.js
+++ src/index.js
@@ -11,13 +11,70 @@ const data = [
     {
         id: 1,
         title: "Adios",
-        isCompleted: false
+        isCompleted: true
     },
 ]
-// document.querySelector("body").appendChild(TodoItem("Hola que tal", ))
+let tasks = data
 
-const todosItems = data.map(d => TodoItem(d.title))
-console.log(Object.entries(todosItems))
-console.log(Object.entries(todosItems)[0][1])
+const completedTasks = tasks.filter(d => d.isCompleted === true)
+const pendingTasks = tasks.filter(d => d.isCompleted === false)
 
-document.querySelector("body").appendChild(Object.entries(todosItems)[0][1])
+
+let nextTask = ""
+let nextId = 2
+
+// elems
+const body = document.querySelector("body")
+const pendingTasksSection = document.createElement("section")
+const pendingTasksHeading = document.createElement("h2")
+const completedTasksSection = document.createElement("section")
+const completedTasksHeading = document.createElement("h2")
+const addNewTaskInput = document.createElement("input")
+const addNewTaskButton = document.createElement("button")
+
+
+body.appendChild(addNewTaskInput)
+body.appendChild(addNewTaskButton)
+body.appendChild(pendingTasksSection)
+body.appendChild(completedTasksSection)
+
+pendingTasksSection.appendChild(pendingTasksHeading)
+completedTasksSection.appendChild(completedTasksHeading)
+
+
+addNewTaskButton.innerText = "Add task"
+pendingTasksHeading.innerText = "Pending tasks"
+completedTasksHeading.innerText = "Completed tasks"
+
+pendingTasks.forEach(task => {
+    pendingTasksSection.appendChild(TodoItem(task, handleTodoChange))
+})
+
+completedTasks.forEach(task => {
+    completedTasksSection.appendChild(TodoItem(task, handleTodoChange))
+})
+
+
+
+function handleTodoChange(value) {
+    console.log(value)
+}
+
+function handleAddTaskInput(e) {
+    nextTask = e.target.value
+}
+
+function handleAddTaskClick() {
+
+    // Add new task to tasks
+    tasks = [...tasks, { id: nextId++, title: nextTask, isCompleted: false }]
+    console.table(tasks)
+    // Create new todoItem
+    // Update task list UI
+    // Trigger a rerender
+}
+
+addNewTaskInput.addEventListener("input", handleAddTaskInput)
+
+addNewTaskButton.addEventListener("click", handleAddTaskClick)
+/* document.querySelector(`[data-todo="0"]`) */
